c252c4324c4422ab719c304bc96ebae060218f3b
cxycxy11 <cxycxy1111@gmail.com>
2018-09-29 09:23:11 +0800

    20180929新增

---
+++ admin/src/main/java/com/alfredteng/casetrace/timeline/TimelineInfoActivity.java
@@ -0,0 +1,261 @@
+package com.alfredteng.casetrace.timeline;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.alfredteng.casetrace.R;
+import com.alfredteng.casetrace.util.BaseActivity;
+import com.alfredteng.casetrace.util.Tool;
+import com.alfredteng.casetrace.util.ViewHandler;
+
+import io.github.mthli.knife.KnifeText;
+
+public class TimelineInfoActivity extends BaseActivity {
+
+    private Toolbar toolbar;
+    private KnifeText knife;
+    private boolean isAdd = false;
+    private long id;
+    private long event_id;
+    private String title;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_timeline);
+        ViewHandler.initToolbarWithBackButton(this,toolbar, R.string.toolbar_tilte_timeline_add);
+        knife = (KnifeText)findViewById(R.id.kt_a_add_timeline);
+        knife.setSelection(knife.getEditableText().length());
+        isAdd = getIntent().getBooleanExtra("isAdd",false);
+        if (isAdd) {
+            if (!Tool.getStringFromPref(this,"timeline_draft","content").equals("")) {
+                knife.fromHtml(Tool.getStringFromPref(this,"timeline_draft","body"));
+            }
+        }else {
+            String content = getIntent().getStringExtra("content");
+        }
+        setupBold();
+        setupItalic();
+        setupUnderline();
+        setupStrikethrough();
+        setupBullet();
+        setupQuote();
+        setupLink();
+        setupClear();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(1,101,1,"保存");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                saveBeforeQuit();
+                this.finish();
+                break;
+            case 101:
+                submit();
+            default:break;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case 1:
+                switch (resultCode) {
+                    case 1:
+                        break;
+                    default:break;
+                }
+                break;
+            default:break;
+        }
+    }
+
+    private void setupBold() {
+        ImageButton bold = (ImageButton) findViewById(R.id.bold_a_add_timeline);
+
+        bold.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                knife.bold(!knife.contains(KnifeText.FORMAT_BOLD));
+            }
+        });
+
+        bold.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(TimelineInfoActivity.this, R.string.app_name, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+
+    private void setupItalic() {
+        ImageButton italic = (ImageButton) findViewById(R.id.italic_a_add_timeline);
+
+        italic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                knife.italic(!knife.contains(KnifeText.FORMAT_ITALIC));
+            }
+        });
+
+        italic.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(TimelineInfoActivity.this, R.string.app_name, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+
+    private void setupUnderline() {
+        ImageButton underline = (ImageButton) findViewById(R.id.underline_a_add_timeline);
+
+        underline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                knife.underline(!knife.contains(KnifeText.FORMAT_UNDERLINED));
+            }
+        });
+
+        underline.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(TimelineInfoActivity.this, R.string.app_name, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+
+    private void setupStrikethrough() {
+        ImageButton strikethrough = (ImageButton) findViewById(R.id.strikethrough_a_add_timeline);
+
+        strikethrough.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                knife.strikethrough(!knife.contains(KnifeText.FORMAT_STRIKETHROUGH));
+            }
+        });
+
+        strikethrough.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(TimelineInfoActivity.this, R.string.app_name, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+
+    private void setupBullet() {
+        ImageButton bullet = (ImageButton) findViewById(R.id.bullet_a_add_timeline);
+
+        bullet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                knife.bullet(!knife.contains(KnifeText.FORMAT_BULLET));
+            }
+        });
+
+
+        bullet.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(TimelineInfoActivity.this, R.string.app_name, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+
+    private void setupQuote() {
+        ImageButton quote = (ImageButton) findViewById(R.id.quote_a_add_timeline);
+
+        quote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                knife.quote(!knife.contains(KnifeText.FORMAT_QUOTE));
+            }
+        });
+
+        quote.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(TimelineInfoActivity.this, R.string.app_name, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+
+    private void setupLink() {
+        ImageButton link = (ImageButton) findViewById(R.id.link_a_add_timeline);
+
+        link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        link.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(TimelineInfoActivity.this, R.string.app_name, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+
+    private void setupClear() {
+        ImageButton clear = (ImageButton) findViewById(R.id.clear_a_add_timeline);
+
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                knife.clearFormats();
+            }
+        });
+
+        clear.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(TimelineInfoActivity.this, R.string.app_name, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+
+    private void submit() {
+        String body = knife.toHtml();
+        if (body.length() < 5000) {
+            Intent intent = new Intent(TimelineInfoActivity.this,TimelineTitleAndHappenTimeInfoActivity.class);
+            intent.putExtra("content",body);
+            intent.putExtra("isAdd",isAdd);
+            startActivityForResult(intent,1);
+        }else {
+            ViewHandler.toastShow(this,"字数已超过最大限制。");
+        }
+    }
+
+    private void saveBeforeQuit() {
+        if (!knife.toHtml().equals("")) {
+            SharedPreferences sharedPreferences = getSharedPreferences("timeline_draft",MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.clear();
+            editor.putString("content",knife.toHtml());
+            editor.apply();
+        }
+    }
+}
